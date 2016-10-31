c5ae749ffee32de042bc80c777457d1fd90d1c72
ak <adriank09@gmail.com>
2016-10-31 21:24:54 +0000

    Replaced Klein with Slim

--- index.php
+++ index.php
@@ -4,42 +4,53 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-//handle subdir
+// Handle subdir
 $base = dirname($_SERVER['PHP_SELF']);
 
 if (ltrim($base, '/'))
     $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
 
-$klein = new \Klein\Klein();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-$klein->respond('GET', '/courses', function($request, $response){
+$container = new \Slim\Container();
+
+$container['notFoundHandler'] = function ($container) {
+    return function (Request $request, Response $response) use ($container) {
+        
+        $data = [
+            'error' => true,
+            'error_str' => 'You appear to be lost, this route doesn\'t exist',
+            'routes' => [
+                '/courses' => 'Get a list of courses and departments',
+                '/sessions' => 'Get a list of sessions for a given course'
+            ]
+        ];
+
+        return $container['response']->withJson($data);
+    };
+};
+
+$app = new \Slim\App($container);
+
+$app->get('/courses', function (Request $request, Response $response) {
+    
+    $parser = new UoBParser\Parser();
+    $courses = $parser->getCourses();
+
+    return $response->withJson($courses);
+});
+
+$app->get('/sessions', function (Request $request, Response $response) {
+    
+    $dept   = $request->getParam('dept');
+    $course = $request->getParam('course');
+    $level  = $request->getParam('level');
 
     $parser = new UoBParser\Parser();
-    $response->json($parser->getCourses());
+    $sessions = $parser->getSessions($dept, $course, $level);
+
+    return $response->withJson($sessions);
 });
 
-$klein->respond('GET', '/sessions', function($request, $response){
-
-    $dept   = $request->param('dept');
-    $course = $request->param('course');
-    $level  = $request->param('level');
-
-    $parser = new UoBParser\Parser();
-    $response->json($parser->getSessions($dept, $course, $level));
-});
-
-$klein->onHttpError(function($code, $router){
-      
-    $data = [
-        'error' => true,
-        'error_str' => 'You appear to be lost, this route doesn\'t exist',
-        'routes' => [
-            'courses' => 'Get a list of courses and departments',
-            'sessions' => 'Get a list of sessions for a given course'
-        ]
-    ];
-
-    $router->response()->json($data);
-});
-
-$klein->dispatch();
+$app->run();
